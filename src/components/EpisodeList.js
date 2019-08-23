81810c5274834f78f3d71933271d66e567761903
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 13:24:27 -0400

    implemented pagination

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -2,25 +2,36 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 import EpisodeCard from './EpisodeCard';
+import PageMenu from "./Pagination";
 
 export default function EpisodeList() {
     // TODO: Add useState to track data from useEffect
     const [epList, setEpList] = useState([]);
+    const [page, setPage] = useState(1);
     useEffect(() => {
         // TODO: Add API Request here - must run in `useEffect`
         //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-        axios.get(`https://rickandmortyapi.com/api/episode/`)
+        axios.get(`https://rickandmortyapi.com/api/episode/?page=${page}`)
             .then(resp => setEpList(resp.data.results))
             .catch(error => console.log("Axios Episodes Error: ", error))
-    }, []);
+    }, [page]);
+
+    const handlePageChange = (number) => {
+        setPage(number);
+    }
 
     return (
-        <section className="episode-list grid-view">
-            {epList.map(ep => {
-                return <EpisodeCard key={ep.id} name={ep.name}
-                    episode={ep.episode} airdate={ep.air_date}
-                    characters={ep.characters.length} />
-            })}
-        </section>
+        <>
+            <section className="episode-list grid-view">
+                {epList.map(ep => {
+                    return <EpisodeCard key={ep.id} name={ep.name}
+                        episode={ep.episode} airdate={ep.air_date}
+                        characters={ep.characters.length} />
+                })}
+            </section>
+            <div>
+                <PageMenu pages={2} handlePageChange={handlePageChange} />
+            </div>
+        </>
     );
 }
