cdbe416eae730742dd15b732b9f996bdf4194e1f
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 11:19:38 -0400

    added tabs to page

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,7 +5,15 @@ import { NavLink } from "react-router-dom";
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
 
+const panes = [
+    { menuItem: 'Home', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+]
+
+export default function TabNav() {
+    return <Tab panes={panes} />
 };
 
