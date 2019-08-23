691b51526a4285ee7b3b1d60ca74828e2c0544f2
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 12:11:52 -0400

    locationList and locationCard now work and display content

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Card, Icon } from 'semantic-ui-react';
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
-}
+const LocationCard = (props) => (
+  <Card>
+    <Card.Content header={props.name} />
+    <Card.Content description={`${props.type} - ${props.dimension}`} />
+    <Card.Content extra>
+      <Icon name='user' />{props.residents} Residents
+    </Card.Content>
+  </Card>
+)
+
+export default LocationCard
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,23 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios"
 
-export default function LocationsList() {}
+import LocationCard from "./LocationCard";
+
+export default function LocationsList() {
+    const [locList, setLocList] = useState([]);
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/location/')
+            .then(resp => setLocList(resp.data.results))
+            .catch(error => console.log("Axios Location Error: ", error))
+    }, [])
+
+    return (
+        <section className="location-list grid-view">
+            {locList.map(location => {
+                return <LocationCard key={location.id} name={location.name}
+                    type={location.type} dimension={location.dimension}
+                    residents={location.residents.length} />
+            })}
+        </section>
+    );
+}
