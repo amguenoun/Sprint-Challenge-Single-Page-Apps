804ec23cbea0d6287c2168c9b878976601b99d19
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 11:28:51 -0400

    changed tab to menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import { Route } from 'react-router-dom'
 
 export default function App() {
   return (
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
+import WelcomePage from "./WelcomePage"
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 
-const panes = [
-    { menuItem: 'Home', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: 'Characters', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Locations', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-    { menuItem: 'Episodes', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-]
+export default class MenuBasic extends Component {
+    state = {}
 
-export default function TabNav() {
-    return <Tab panes={panes} />
-};
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <Menu>
+                <Menu.Item
+                    name='Home'
+                    active={activeItem === 'Home'}
+                    onClick={this.handleItemClick}
+                >
+                    Home
+                </Menu.Item>
+
+                <Menu.Item
+                    name='Characters'
+                    active={activeItem === 'Characters'}
+                    onClick={this.handleItemClick}
+                >
+                    Characters
+                </Menu.Item>
+
+                <Menu.Item
+                    name='Location'
+                    active={activeItem === 'Location'}
+                    onClick={this.handleItemClick}
+                >
+                    Location
+                </Menu.Item>
+
+                <Menu.Item
+                    name='Episodes'
+                    active={activeItem === 'Episodes'}
+                    onClick={this.handleItemClick}
+                >
+                    Episodes
+                </Menu.Item>
+            </Menu>
+        )
+    }
+}
 
