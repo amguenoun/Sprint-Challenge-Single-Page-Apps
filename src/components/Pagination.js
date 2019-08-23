62f608c33b18aa8905e432ac2cffb7f0a8236294
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 13:32:42 -0400

    centered pagination menu on page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,7 +3,9 @@ import { Pagination } from 'semantic-ui-react'
 
 const PageMenu = (props) => {
     return (
-        <Pagination defaultActivePage={1} totalPages={props.pages} onPageChange={(event, data) => props.handlePageChange(data.activePage)} />
+        <div className="pages">
+            <Pagination defaultActivePage={1} totalPages={props.pages} onPageChange={(event, data) => props.handlePageChange(data.activePage)} />
+        </div>
     );
 }
 
