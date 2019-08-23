858f8989dbe29365e96296a61650a042a22532a5
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-23 12:28:33 -0400

    episodeList and episodeCard components now fully functional

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
 
-export default function EpisodeCard() {
-    return <span>todo: episode</span>;
-}
+const EpisodeCard = (props) => (
+    <Card>
+        <Card.Content header={props.name} />
+        <Card.Content description={`${props.episode} - ${props.airdate}`} />
+        <Card.Content extra>
+            <Icon name='user' />{props.characters} Characters
+    </Card.Content>
+    </Card>
+)
+
+export default EpisodeCard
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import EpisodeCard from './EpisodeCard';
 
 export default function EpisodeList() {
     // TODO: Add useState to track data from useEffect
-
+    const [epList, setEpList] = useState([]);
     useEffect(() => {
         // TODO: Add API Request here - must run in `useEffect`
         //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        axios.get(`https://rickandmortyapi.com/api/episode/`)
+            .then(resp => setEpList(resp.data.results))
+            .catch(error => console.log("Axios Episodes Error: ", error))
     }, []);
 
     return (
         <section className="episode-list grid-view">
-            <h2>TODO: `array.map()` over your state here!</h2>
+            {epList.map(ep => {
+                return <EpisodeCard key={ep.id} name={ep.name}
+                    episode={ep.episode} airdate={ep.air_date}
+                    characters={ep.characters.length} />
+            })}
         </section>
     );
 }
